968519f45d967e79baf6a8644115770695f96791
Rahul Sharma <rahul5982439@gmail.com>
2025-02-24 21:34:57 +0530

    Updated start script

--- byoeb-v1/start.sh
+++ byoeb-v1/start.sh
@@ -1,20 +1,16 @@
-pip install poetry
+curl -sSL https://install.python-poetry.org | python3 - --version 2.1.1
+echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
 poetry config virtualenvs.in-project true
 
 cd byoeb-v1/byoeb-core
 poetry install --no-interaction
 poetry build
 
-cd ..
-cd byoeb-integrations
+cd ../byoeb-integrations
 poetry install --no-interaction
-poetry add ../byoeb-core/dist/byoeb_core-0.1.0-py3-none-any.whl 
 poetry build
 
-cd ..
-cd byoeb
+cd ../byoeb
 poetry install --no-interaction
-poetry add ../byoeb-integrations/dist/byoeb_integrations-0.1.0-py3-none-any.whl
-poetry env activate
+eval "$(poetry env activate)"
 python byoeb/chat_app/run.py
-
