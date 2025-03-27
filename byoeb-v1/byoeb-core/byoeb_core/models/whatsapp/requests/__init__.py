d56fd05e114f78ba68ded672e195f455dea18347
Rahul Sharma <rahul5982439@gmail.com>
2025-03-28 03:18:13 +0530

    Added WhatsAppVideo class to init

--- byoeb-v1/byoeb-core/byoeb_core/models/whatsapp/requests/__init__.py
+++ byoeb-v1/byoeb-core/byoeb_core/models/whatsapp/requests/__init__.py
@@ -1,7 +1,7 @@
 from byoeb_core.models.whatsapp.requests.message_request import WhatsAppMessage, Text, Reaction, Audio
 from byoeb_core.models.whatsapp.requests.interactive_message_request import WhatsAppInteractiveMessage, Interactive, InteractiveBody, InteractiveAction, InteractiveReply, InteractiveActionButton, InteractiveActionSection, InteractiveSectionRow, InteractiveMessageTypes
 from byoeb_core.models.whatsapp.requests.template_message_request import WhatsAppTemplateMessage, Template, TemplateComponent, TemplateLanguage, TemplateParameter
-from byoeb_core.models.whatsapp.requests.media_request import WhatsAppMediaMessage, WhatsAppAudio, MediaData
+from byoeb_core.models.whatsapp.requests.media_request import WhatsAppMediaMessage, WhatsAppAudio, MediaData, WhatsAppVideo
 from byoeb_core.models.whatsapp.requests.read_receipt_request import WhatsAppReadMessage
 
 __all__ = [
@@ -25,6 +25,7 @@ __all__ = [
     'TemplateParameter',
     'WhatsAppMediaMessage',
     'WhatsAppAudio',
+    'WhatsAppVideo',
     'MediaData',
     'WhatsAppReadMessage'
 ]
