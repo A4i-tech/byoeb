8ee89312e0998fa8ec9bab0be0c220362a3eddfc
N1kunj1998 <njkhakhkhar@gmail.com>
2025-08-26 14:00:32 +0530

    ci: remove dependency caching & MongoDB service from github actions; tests: add in-memory DB mocking for mongodb

--- byoeb-v1/byoeb-integrations/byoeb_integrations/databases/tests/test_async_azure_cosmos_mongo_db.py
+++ byoeb-v1/byoeb-integrations/byoeb_integrations/databases/tests/test_async_azure_cosmos_mongo_db.py
@@ -5,6 +5,9 @@ import pytest
 from byoeb_integrations.databases.mongo_db.azure.async_azure_cosmos_mongo_db import AsyncAzureCosmosMongoDB, AsyncAzureCosmosMongoDBCollection, is_tls_enabled
 from byoeb_integrations import test_environment_path
 from dotenv import load_dotenv
+from collections import defaultdict
+import sys
+import uuid
 
 load_dotenv(test_environment_path)
 connection_string = "mongodb://localhost:27017/?tls=false"
@@ -12,6 +15,113 @@ db_name = "test_new_frame"
 
 c1 = "c1"
 c2 = "c2"
+
+@pytest.fixture(autouse=True)
+def fake_cosmos_mongo(mocker):
+    """Autouse: patch DB classes with in-memory fakes (no real DB)."""
+    store = defaultdict(dict)  # {collection_name: {_id: doc}}
+
+    def _match(doc, query):
+        if not query:
+            return True
+        for k, v in query.items():
+            if isinstance(v, dict):
+                if "$gte" in v:
+                    if k not in doc or not (doc[k] >= v["$gte"]):
+                        return False
+                else:
+                    return False
+            else:
+                if doc.get(k) != v:
+                    return False
+        return True
+
+    class _FakeCollectionHandle:
+        def __init__(self, name):
+            self.name = name
+            store.setdefault(name, {})
+
+    class FakeAsyncAzureCosmosMongoDB:
+        def __init__(self, connection_string, db_name):
+            self.connection_string = connection_string
+            self.db_name = db_name
+
+        async def adelete_collection(self, name: str):
+            store.pop(name, None)
+
+        def get_collection(self, name: str):
+            return _FakeCollectionHandle(name)
+
+        async def adelete_database(self):
+            store.clear()
+
+    class FakeAsyncAzureCosmosMongoDBCollection:
+        def __init__(self, collection_handle: _FakeCollectionHandle):
+            self._name = collection_handle.name
+
+        async def ainsert(self, documents):
+            col = store[self._name]
+            cnt = 0
+            for d in documents:
+                d = dict(d)
+                d.setdefault("_id", str(uuid.uuid4()))
+                col[d["_id"]] = d
+                cnt += 1
+            return {"inserted_count": cnt}
+
+        async def afetch_all(self, query=None):
+            query = query or {}
+            col = store[self._name]
+            return [dict(doc) for doc in col.values() if _match(doc, query)]
+
+        async def afetch(self, query):
+            items = await self.afetch_all(query)
+            return items[0] if items else None
+
+        async def afetch_ids(self):
+            return list(store[self._name].keys())
+
+        async def aupdate(self, filter=None, update=None, bulk_queries=None):
+            updates = []
+            if bulk_queries:
+                updates.extend(bulk_queries)      # list[(filter, update)]
+            elif filter is not None and update is not None:
+                updates.append((filter, update))
+
+            modified = 0
+            col = store[self._name]
+            for f, u in updates:
+                for _id, doc in list(col.items()):
+                    if _match(doc, f):
+                        if "$set" in u:
+                            doc.update(u["$set"])
+                            col[_id] = doc
+                            modified += 1
+            return {"modified_count": modified}, modified
+
+        async def adelete(self, filter=None, bulk_queries=None):
+            filters = []
+            if bulk_queries:
+                filters.extend(bulk_queries)       # list[filter]
+            elif filter:
+                filters.append(filter)
+
+            deleted = 0
+            col = store[self._name]
+            for f in filters:
+                for _id, doc in list(col.items()):
+                    if _match(doc, f):
+                        del col[_id]
+                        deleted += 1
+            return {"deleted_count": deleted}, deleted
+
+        async def adelete_collection(self):
+            store.pop(self._name, None)
+
+    # Patch the names imported into THIS test file
+    current_mod = sys.modules[__name__]
+    mocker.patch.object(current_mod, "AsyncAzureCosmosMongoDB", FakeAsyncAzureCosmosMongoDB)
+    mocker.patch.object(current_mod, "AsyncAzureCosmosMongoDBCollection", FakeAsyncAzureCosmosMongoDBCollection)
 
 @pytest.fixture(scope="session")
 def event_loop():
