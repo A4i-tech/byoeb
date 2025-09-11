1cec6132c0ba8729f4565e260dc6eff999b092c7
N1kunj1998 <97668145+N1kunj1998@users.noreply.github.com>
2025-09-11 13:28:23 +0530

    A4i/main add others user (#3)
    * feat: add data extraction script for ASHABot user mapping
    
    * feat(onboarding): add Others user category with onboarding tests
    
    * added code for running pytest of byoeb
    
    * refactored code with seperating constant messages and added strategy design pattern for type of user creation
    
    * made changes in aregister method of user handler to follow DRY approach for testing
    
    * made change in tests.yml file to run byoeb test
    
    * added code for coverage of ./byoeb-v1/byoeb tests
    
    * corrected code for testing coverage test
    
    * added coverage test in pyproject.toml file

---
+++ byoeb-v1/byoeb/byoeb/constants/user_enums.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class UserType(Enum):
+    ASHA = "asha"
+    ANM = "anm"
+    OTHERS = "others"
+
+class LanguageCode(Enum):
+    HINDI = "hi"
+    ENGLISH = "en"
+    MARATHI = "mr"
+    TELUGU = "te"
