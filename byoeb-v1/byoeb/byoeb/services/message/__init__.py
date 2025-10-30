f60f8ecb15b7101dcdacb47af989dd2852ec0e18
N1kunj1998 <njkhakhkhar@gmail.com>
2025-10-30 13:27:57 +0530

    removed unnecessary init files

--- byoeb-v1/byoeb/byoeb/services/message/__init__.py
+++
@@ -1,3 +0,0 @@
-# Message services package - MessageService has been removed
-# All functionality moved to byoeb.services.databases.mongo_db.MessageMongoDBService
-__all__ = []
--- byoeb-v1/byoeb/byoeb/services/user/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-User service module - UserService has been removed
-All functionality moved to byoeb.services.databases.mongo_db.UserMongoDBService
-"""
-__all__ = []
