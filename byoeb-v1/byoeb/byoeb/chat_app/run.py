defce9eaae153247dc23553a781031314d788176
Muqsit <notmuqsit+a4i@gmail.com>
2025-11-25 14:25:02 +0530

    ci/cd: use same staging-deploy environment as azure webapp (#83)
    * ci-cd: temporarily enlist muqsit/docker-azure-env as staging branch
    
    * Dockerfile: use python:3.11-slim-bullseye
    This commit is expected to cause a deployment failure
    
    * chat-app: overwrite default sqlite3 library with pysqlite3-binary
    Expecting a successful deployment...
    
    * Revert "ci-cd: temporarily enlist muqsit/docker-azure-env as staging branch"
    
    This reverts commit 4fb32fb6fb353d4b6cad18737266f9d7421a02ad.

--- byoeb-v1/byoeb/byoeb/chat_app/run.py
+++ byoeb-v1/byoeb/byoeb/chat_app/run.py
@@ -1,3 +1,9 @@
+# fixes crash during 'import chromadb' - see: https://docs.trychroma.com/docs/overview/troubleshooting#sqlite
+import sys
+__import__("pysqlite3")
+sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+
+
 import logging
 import logging.config
 import os
