8eb7ae5fad6131dbceab145cfcf1365fa7d2ba70
Muqsit <notmuqsit+a4i@gmail.com>
2025-11-28 16:38:15 +0800

    related-questions: improve error correction logic by feeding LLM its errors

--- byoeb-v1/byoeb-integrations/byoeb_integrations/vector_stores/related_questions.py
+++ byoeb-v1/byoeb-integrations/byoeb_integrations/vector_stores/related_questions.py
@@ -1,14 +1,18 @@
 import asyncio
 import json
 import re
-from typing import List, Optional
+from typing import Dict, List, Optional
 from byoeb_core.llms.base import BaseLLM
 from byoeb_core.vector_stores.base import BaseVectorStore
 from pydantic import TypeAdapter
 from tenacity import retry, stop_after_attempt, wait_fixed
 
+
 @retry(stop=stop_after_attempt(3), wait=wait_fixed(1))
-async def aget_search_queries(text: str, llm_client: BaseLLM) -> List[str]:
+async def _aget_search_queries(text: str, llm_client: BaseLLM) -> List[str]:
+    """
+    Curate 'related questions' queries for vector search lookup.
+    """
     prompt = [{"role": "user", "content": (
         "Produce up to 3 short search keywords. "
         "Each query should identify topics that are somewhat related to the text, "
@@ -23,29 +27,57 @@ async def aget_search_queries(text: str, llm_client: BaseLLM) -> List[str]:
     return TypeAdapter(List[str]).validate_python(words)
 
 
-async def aget_related_questions(
-    text,
-    llm_client: BaseLLM,
-    languages_translation_prompts: dict,
-    vector_store: Optional[BaseVectorStore] = None,
-    system_prompt = None,
-    length: int = 60
-):
-    related_chunks = []
-    if vector_store:
-        from byoeb_integrations.vector_stores.azure_vector_search.azure_vector_search import AzureVectorSearchType
-        queries = await aget_search_queries(text, llm_client)
-        results = await asyncio.gather(*[vector_store.aretrieve_top_k_chunks(
-            q,
-            k=2,
-            search_type=AzureVectorSearchType.DENSE.value,
-            select=["id", "text"],
-            vector_field="text_vector_3072"
-        ) for q in queries])
-        related_chunks = [c.text for r in results for c in r]
+async def _aget_related_questions(llm_client: BaseLLM, system_prompt: str, user_prompt: str, length: int) -> List[str]:
+    resp = None
+    error = None
+    for _ in range(3):
+        prompts = [
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt}
+        ]
+        if error:
+            # empirically, this works better than tenacity retries - the LLM is placed in a feedback loop
+            # where it gets an opportunity to correct its mistake.
+            prompts.append({"role": "assistant", "content": resp})
+            prompts.append({"role": "user", "content": error})
 
-    related_questions_dict = {}
+        _, resp = await llm_client.agenerate_response(prompts)
+
+        related_questions = re.findall(r"<q_\d+>(.*?)</q_\d+>", resp)
+        for question in related_questions:
+            if len(question) > length:
+                error = f"Related question too long: {len(question)} > {length}"
+                continue
+
+        error = None
+        break
+
+    if error is not None: raise ValueError(error)
+    return related_questions
+
+
+async def aget_related_questions(
+    text: str,
+    llm_client: BaseLLM,
+    languages_translation_prompts: Dict[str, str],
+    vector_store: Optional[BaseVectorStore] = None,
+    system_prompt: Optional[str] = None,
+    length: int = 60
+) -> Dict[str, List[str]]:
     if not system_prompt:
+        related_chunks = []
+        if vector_store:
+            from byoeb_integrations.vector_stores.azure_vector_search.azure_vector_search import AzureVectorSearchType
+            queries = await _aget_search_queries(text, llm_client)
+            results = await asyncio.gather(*[vector_store.aretrieve_top_k_chunks(
+                q,
+                k=2,
+                search_type=AzureVectorSearchType.DENSE.value,
+                select=["id", "text"],
+                vector_field="text_vector_3072"
+            ) for q in queries])
+            related_chunks = [c.text for r in results for c in r]
+
         related_chunks_text = "\n\n".join(related_chunks)
         system_prompt = (
             "Generate three related questions for the given text. "
@@ -67,27 +99,19 @@ async def aget_related_questions(
             "</instructions>"
         )
 
-    prompt = [{"role": "system", "content": system_prompt}]
-    prompt.append({"role": "user", "content": text})
-    llm_response, resp = await llm_client.agenerate_response(prompt)
-    related_questions = re.findall(r"<q_\d+>(.*?)</q_\d+>", resp)
-    related_questions_dict["en"] = related_questions
+    related_questions = {"en": await _aget_related_questions(llm_client, system_prompt, text, length)}
 
+    user_prompt = f"""Translate the following list of questions <en_questions> {related_questions['en']} </en_questions> from english to desired language.
+    Maintain the output structure as follows:
+    <related_questions>
+    <q_1>Translated question 1</q_1>
+    <q_2>Translated question 2</q_2>
+    <q_3>Translated question 3</q_3>
+    </related_questions>
+    Note above is a sample for three questions follow same based on number of questions.
+    """
     for lang, translation_prompt in languages_translation_prompts.items():
-        user_prompt = f"""Translate the following list of questions <en_questions> {related_questions} </en_questions> from english to desired language.
-        Maintain the output structure as follows:
-        <related_questions>
-        <q_1>Translated question 1</q_1>
-        <q_2>Translated question 2</q_2>
-        <q_3>Translated question 3</q_3>
-        </related_questions>
-        Note above is a sample for three questions follow same based on number of questions.
-        """
-        prompt = [{"role": "system", "content": translation_prompt}]
-        prompt.append({"role": "user", "content": user_prompt})
-        llm_response, resp = await llm_client.agenerate_response(prompt)
-        related_questions = re.findall(r"<q_\d+>(.*?)</q_\d+>", resp)
-        related_questions_dict[lang] = related_questions
-    
-    return related_questions_dict
+        related_questions[lang] = await _aget_related_questions(llm_client, translation_prompt, user_prompt, length)
+
+    return related_questions
         
