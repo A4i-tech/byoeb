7ca083d45471ba35cdfea6e7e03425db5428a57b
Muqsit <notmuqsit+a4i@gmail.com>
2025-12-17 20:07:21 +0800

    fix failing test case We shouldn't be using dependency_setup.py in our unit tests, but this is sufficient for time being

--- byoeb-v1/byoeb/tests/conftest.py
+++ byoeb-v1/byoeb/tests/conftest.py
@@ -2,3 +2,7 @@
 import sys
 __import__("pysqlite3")
 sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+
+import os
+os.environ["AZURE_OPENAI_API_KEY"] = "sk-xxxx"
+os.environ["AZURE_OPENAI_SPEECH_ENDPOINT"] = "http://localhost:8000"
